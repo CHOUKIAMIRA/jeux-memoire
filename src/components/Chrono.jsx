b0c34919df170bfbb0d55c2e6d83ecd1a9e33e2b
amira <choukiamira77@gmail.com>
2024-09-26 20:59:39 +0100

    added commit

---
+++ src/components/Chrono.jsx
@@ -0,0 +1,12 @@
+
+
+function Chrono({ time,score }) {
+  return (
+   <div  >
+      <h2 style={{color:"white",textAlign:"center"}}>Chronométre : {time} </h2>
+      <h2 style={{color:"white",textAlign:"center"}}>Score : {score} </h2>
+    </div>
+  );
+}
+
+export default Chrono;
